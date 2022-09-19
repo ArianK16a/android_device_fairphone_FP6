8095ad8b2f9a2c9cd23229632bc6aff5d450b930
Chirayu Desai <chirayudesai1@gmail.com>
2022-09-20 04:09:01 +0530

    FP6: Set BOOT_SECURITY_PATCH
    * This is put in vbmeta, and Fairphone OS sets it
    
    Change-Id: I69485c8207eca5ee9e7f1f4e02d684707c6dece3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -199,7 +199,8 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2025-11-05
+BOOT_SECURITY_PATCH := 2025-11-05
+VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
