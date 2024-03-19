bdd49866ef93c0b2c9fd6b1cbdde083fca4f51df
Arian <arian.kulmer@web.de>
2024-03-19 23:12:35 +0100

    FP6: Build GNSS HAL from source and import configs from stock
    Change-Id: I5da8b02daf11a84d5083810ab3bcbe613cf618bb

--- device.mk
+++ device.mk
@@ -64,6 +64,40 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.qcom \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
 
+# GPS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@2.1-impl-qti \
+    android.hardware.gnss-aidl-impl-qti \
+    android.hardware.gnss-aidl-service-qti
+
+PRODUCT_PACKAGES += \
+    libbatching \
+    libgeofencing \
+    libgnss
+
+PRODUCT_PACKAGES += \
+    apdr.conf \
+    batching.conf \
+    gnss_antenna_info.conf \
+    gps.conf \
+    izat.conf \
+    lowi.conf \
+    sap.conf \
+    xtwifi.conf
+
+PRODUCT_PACKAGES += \
+    gnss@2.0-base.policy \
+    gnss@2.0-xtra-daemon.policy \
+    gnss@2.0-qsap-location.policy \
+    gnss@2.0-xtwifi-client.policy \
+    gnss@2.0-edgnss-daemon.policy
+
+$(call soong_config_set, qtilocation, feature_locauto, false)
+$(call soong_config_set, qtilocation, feature_nhz, false)
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
+
 # Overlays
 PRODUCT_PACKAGES += \
     FrameworksResCommon_Sys \
