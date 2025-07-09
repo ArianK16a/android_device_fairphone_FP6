3eab17fa2c45ee375d1d6b8adc2fdcaad16e57b2
basamaryan <basam.aryan@gmail.com>
2025-07-09 08:52:21 -0400

    FP6: Update WFD system blobs from dada OS2.0.217.0.WOCMIXM
    Change-Id: I95dfb07cae97f3c8716a7cb6d87a7081de986748

--- extract-files.py
+++ extract-files.py
@@ -92,11 +92,6 @@ def blob_fixup_sql_reorder(
 
 
 blob_fixups: blob_fixups_user_type = {
-    'system_ext/lib64/libwfdservice.so': blob_fixup()
-        .replace_needed(
-            'android.media.audio.common.types-V2-cpp.so',
-            'android.media.audio.common.types-V4-cpp.so',
-        ),
     (
         'vendor/bin/qcc-vendor',
         'vendor/bin/qms',
