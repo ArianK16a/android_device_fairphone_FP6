01c2bf30b69b61051cd617c5b8f79aad0de1bc8a
Arian <arian.kulmer@web.de>
2025-07-10 22:02:16 +0200

    FP6: Configure architecture for volcano
    Change-Id: I0acbfda60438c2f2cdff1e07b1e1d2b62d486d96

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,5 +5,12 @@
 
 DEVICE_PATH := device/fairphone/FP6
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a-branchprot
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := kryo300
+
 # Inherit the proprietary files BoardConfig
 include vendor/fairphone/FP6/BoardConfigVendor.mk
