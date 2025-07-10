9f24cd060f082674850bbb6950ca9ad5acfe2784
Arian <arian.kulmer@web.de>
2025-07-10 22:06:14 +0200

    FP6: Configure bootloader and board names
    Change-Id: Ie86f74d525ba1bf526b64fbbcd8032888bfceb90

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,5 +12,9 @@ TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := kryo
 
+# Board
+TARGET_BOARD_PLATFORM := volcano
+TARGET_BOOTLOADER_BOARD_NAME := fps
+
 # Inherit the proprietary files BoardConfig
 include vendor/fairphone/FP6/BoardConfigVendor.mk
