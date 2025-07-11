d00989b0247ca6658950b3ab2016ad9102a21afa
Arian <arian.kulmer@web.de>
2025-07-11 13:28:38 +0200

    FP6: Set shipping board and product api level
    Change-Id: Icfad1a55430823c75a4b426d0ded8118310bb674

--- device.mk
+++ device.mk
@@ -24,6 +24,10 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Enable virtualization service
 $(call inherit-product, packages/modules/Virtualization/apex/product_packages.mk)
 
+# API
+PRODUCT_SHIPPING_API_LEVEL := 35
+BOARD_SHIPPING_API_LEVEL := 34
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
