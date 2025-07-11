5d14061e80bded358e2aa20bc1ba9e53a1fd98a1
Arian <arian.kulmer@web.de>
2025-07-11 15:04:44 +0200

    FP6: Setup VINTF manifests
    Import manifest_volcano from LA.VENDOR.14.3.0.r1-21700-lanai.0.
    Add framework_manifest.xml from qssi but only keep qccsyshal.
    
    Change-Id: I06ca66e48d2e78e49fd9e3c0e1d3d3fccdc8d8db

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -187,5 +187,29 @@ BOARD_AVB_INIT_BOOT_ALGORITHM := SHA256_RSA2048
 BOARD_AVB_INIT_BOOT_ROLLBACK_INDEX := $(STOCK_SECURITY_PATCH_TIMESTAMP)
 BOARD_AVB_INIT_BOOT_ROLLBACK_INDEX_LOCATION := 4
 
+# VINTF
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
+
+DEVICE_MANIFEST_SKUS := taro diwali cape ukee
+$(foreach sku, $(call to-upper, $(DEVICE_MANIFEST_SKUS)), \
+    $(eval DEVICE_MANIFEST_$(sku)_FILES := \
+        $(COMMON_PATH)/vintf/manifest.xml \
+        $(COMMON_PATH)/vintf/manifest_xiaomi.xml \
+        $(if $(TARGET_NFC_SUPPORTED_SKUS),$(COMMON_PATH)/vintf/manifest_no_nfc.xml,) \
+    ))
+
+ifneq ($(TARGET_NFC_SUPPORTED_SKUS),)
+ODM_MANIFEST_SKUS += $(TARGET_NFC_SUPPORTED_SKUS)
+$(foreach nfc_sku, $(call to-upper, $(TARGET_NFC_SUPPORTED_SKUS)), \
+    $(eval ODM_MANIFEST_$(nfc_sku)_FILES += $(COMMON_PATH)/vintf/manifest_nfc.xml))
+endif
+
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
+
+DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/vintf/framework_manifest.xml
+
 # Inherit the proprietary files BoardConfig
 include vendor/fairphone/FP6/BoardConfigVendor.mk
