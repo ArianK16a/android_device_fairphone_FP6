9dedb48d22be4fcd1ad47128be3f617da9f65f6a
Arian <arian.kulmer@web.de>
2025-07-11 16:15:53 +0200

    FP6: Create mountpoints in vendor
    Change-Id: I6264ea7e44cc3cc6edd5c70032ad8a1248eb2408

--- device.mk
+++ device.mk
@@ -56,6 +56,11 @@ PRODUCT_PACKAGES += \
 $(call soong_config_set, ufsbsg, ufsframework, bsg)
 
 # Partitions
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint
+
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Soong namespaces
