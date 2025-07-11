a0f0d40a335faa0f8ca955575c73d9c636c596e3
Arian <arian.kulmer@web.de>
2025-07-11 13:31:04 +0200

    FP6: Setup virtual A/B partitions
    Change-Id: I9a53838d4c78b34e4972186714b527e7dec617e4

--- device.mk
+++ device.mk
@@ -24,6 +24,23 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Enable virtualization service
 $(call inherit-product, packages/modules/Virtualization/apex/product_packages.mk)
 
+# A/B
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
+
+PRODUCT_PACKAGES += \
+    checkpoint_gc \
+    otapreopt_script
+
 # API
 PRODUCT_SHIPPING_API_LEVEL := 35
 BOARD_SHIPPING_API_LEVEL := 34
@@ -34,6 +51,12 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+
+# Update engine
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
 
 # Inherit from the proprietary files makefile
 $(call inherit-product, vendor/fairphone/FP6/FP6-vendor.mk)
