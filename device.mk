6a1f50eeb99666219b5366bb7182d2558d08f4b4
Arian <arian.kulmer@web.de>
2025-07-23 15:51:43 +0200

    FP6: Build qti display demura service
    Change-Id: I4380c99cf6269be0f252aa807c55b050d78f7df5

--- device.mk
+++ device.mk
@@ -135,7 +135,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     vendor.qti.hardware.display.allocator-service \
-    vendor.qti.hardware.display.composer-service
+    vendor.qti.hardware.display.composer-service \
+    vendor.qti.hardware.display.demura-service
 
 PRODUCT_PACKAGES += \
     init.qti.display_boot.rc \
