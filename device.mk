149f0519a5d525e4bd567e96973d295aa6d77c5c
Arian <arian.kulmer@web.de>
2025-07-24 15:25:37 +0200

    FP6: Copy ipsec_tunnels permission
    Change-Id: Idd00f0f71cc4b4511434c24c02449af6738c6c6c

--- device.mk
+++ device.mk
@@ -236,6 +236,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.memtrack-service
 
+# Network
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
+
 # NFC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
