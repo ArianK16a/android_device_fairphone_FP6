cf81fde5a2330fe869c2a8fbad89a63c9057de45
Arian <arian.kulmer@web.de>
2025-07-24 15:19:43 +0200

    FP6: Copy keystore app attest key permission
    Change-Id: I2af483ce7f5ec6c48cfb2d850374fac2262bea0f

--- device.mk
+++ device.mk
@@ -220,6 +220,10 @@ PRODUCT_PACKAGES += \
     IPACM_cfg.xml \
     IPACM_Filter_cfg.xml
 
+# Keystore
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml
+
 # Overlays
 PRODUCT_PACKAGES += \
     FrameworksResCommon_Sys \
