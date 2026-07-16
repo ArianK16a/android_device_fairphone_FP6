cfcd787077af76a3f7dd0a1f6a7026642a07e5cf
Arian <arian.kulmer@web.de>
2026-07-16 15:18:39 +0200

    TMP permissive selinux
    Change-Id: Id18b96e99b1b049a23380868776fcf8a40784242

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -85,7 +85,8 @@ BOARD_BOOTCONFIG := \
     androidboot.memcg=1 \
     androidboot.usbcontroller=a600000.dwc3 \
     androidboot.load_modules_parallel=true \
-    androidboot.console=0
+    androidboot.console=0 \
+    androidboot.selinux=permissive
 
 # Kernel modules
 BOARD_SYSTEM_KERNEL_MODULES_BLOCKLIST_FILE := $(TARGET_KERNEL_SOURCE)/modules.systemdlkm_blocklist.msm.pineapple
