9fca99a5356eb5dd9316f949415b1a77494c6a3e
Arian <arian.kulmer@web.de>
2026-07-23 19:30:47 +0200

    Revert "TMP Use QMAA usb init for earlier logs"
    This reverts commit fafc71c9500be5e02d8398d8383d03ce3c811145.

--- device.mk
+++ device.mk
@@ -416,10 +416,13 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb-service.qti
+    android.hardware.usb-service.qti \
+    android.hardware.usb.gadget-service.qti
 
 PRODUCT_PACKAGES += \
-    init.qti.usb.qmaa.rc
+    init.qcom.usb.rc \
+    init.qcom.usb.sh \
+    usb_compositions.conf
 
 PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/usb/etc
 
